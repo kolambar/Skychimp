684ea62bd7657a0cf79dd650a12587a9011abdd8
Nikolai <kolamba.r@gmail.com>
2023-08-20 17:24:19 +0300

    added css and js started model and view updated urls

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,25 @@
+from django.utils.text import slugify
+from unidecode import unidecode
 from django.db import models
 
+
 # Create your models here.
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Mailin(models.Model):
+    name = models.CharField(max_length=150, verbose_name='название рассылки', unique=True)
+    slug = models.SlugField(unique=True, blank=True, verbose_name='название рассылки')
+    mail_time = models.TimeField(verbose_name='время рассылки')
+    
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(unidecode(self.name))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name}'
--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from msilib.schema import ListView
+
 
 # Create your views here.
+
+
+class MailingListView(ListView):
+    model = Mailing
