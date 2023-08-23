8f6b2c9d536758a74e25bc75e22427cf6877ed3b
Nikolai <kolamba.r@gmail.com>
2023-08-24 00:06:33 +0300

    admin.py filled

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
+from mailing.models import Mailin, Client, Message, Attempts
+
+
 # Register your models here.
+
+
+@admin.register(Mailin)
+class MailinAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mail_time', 'interval', 'status',)
+    list_filter = ('status',)
+    search_fields = ('name', 'mail_time', 'interval', 'slug',)
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email',)
+    list_filter = ('mailin',)
+    search_fields = ('name', 'email', 'mailin',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mailing',)
+    search_fields = ('name', 'mailin',)
+
+
+@admin.register(Attempts)
+class AttemptsAdmin(admin.ModelAdmin):
+    list_display = ('data_time', 'status', 'comment')
+    list_filter = ('status',)
+    search_fields = ('data_time', 'status', 'comment',)
