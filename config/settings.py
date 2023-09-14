32f4ac475162365caf806daba6c494e4c0d32154
Nikolai <kolamba.r@gmail.com>
2023-09-14 10:51:35 +0300

    add apps users and blog. they need to fix. models have been changed.

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django_crontab',
 
     'mailing',
+    'users',
+    'blog',
 ]
 
 
@@ -134,14 +136,20 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
         BASE_DIR / 'static',
 )
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
-LOGOUT_REDIRECT_URL = '/'
-LOGIN_REDIRECT_URL = '/'
+# LOGOUT_REDIRECT_URL = '/'
+# LOGIN_REDIRECT_URL = '/'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
---
+++ users/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    phone = models.CharField(max_length=20, verbose_name='номер телефона', **NULLABLE)
+    country = models.CharField(max_length=58, verbose_name='название страны', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    verified = models.BooleanField(default=False, verbose_name='верифицирован', blank=True)
+    verified_password = models.IntegerField(verbose_name='ключ для верификации')
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
