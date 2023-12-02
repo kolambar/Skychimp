3c54ecc1ad10a87305d3edef88e4b9a3fd3e0602
Nikolai <kolamba.r@gmail.com>
2023-12-02 18:41:44 +0300

    added several tests for mailing

--- mailing/tests.py
+++ mailing/tests.py
@@ -1,86 +1,115 @@
 import json
 from datetime import datetime
 
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from mailing.models import Message, Mailin
+from mailing.forms import MailinCreateForm
+from mailing.models import Message, Mailin, Client
 from users.models import User
 
 
 class MailinTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create(email='testuser@mail.ru', verified_password=12354)
+        user = User.objects.create(email='testuser@mail.ru', verified_password=12345)
         user.set_password('pythonpass')
         user.save()
+
+        # Зарегистрированный клиент
+        self.client.login(**{'email': 'testuser@mail.ru', 'password': 'pythonpass'})
+
         self.user = User.objects.get(email='testuser@mail.ru')
         self.message = Message.objects.create(name='test1', text='abc', owner=user)
 
-        start_naive_datetime = datetime.strptime('2024-12-01 01:02:00', '%Y-%m-%d %H:%M:%S')
-        end_naive_datetime = datetime.strptime('2025-12-01 01:02:00', '%Y-%m-%d %H:%M:%S')
+        # Формат времени для формы
+        self.start_naive_datetime = datetime.strptime('2024-12-01 01:02:00', '%Y-%m-%d %H:%M:%S')
+        self.end_naive_datetime = datetime.strptime('2025-12-01 01:02:00', '%Y-%m-%d %H:%M:%S')
+        self.start_date_str = self.start_naive_datetime.strftime('%Y-%m-%d')  # Формат даты
+        self.start_time_str = self.start_naive_datetime.strftime('%H:%M:%S')  # Формат времени
+        self.finis_date_str = self.end_naive_datetime.strftime('%Y-%m-%d')  # Формат даты
+        self.finis_time_str = self.end_naive_datetime.strftime('%H:%M:%S')  # Формат времени
 
-        self.start_aware_datetime = timezone.make_aware(start_naive_datetime)
-        self.end_aware_datetime = timezone.make_aware(end_naive_datetime)
+        # формат времени для Django ORM
+        start_aware_datetime = timezone.make_aware(self.start_naive_datetime)
+        end_aware_datetime = timezone.make_aware(self.end_naive_datetime)
 
-        mailin = Mailin.objects.create(name='test_mail_1', interval='daily', start_time=self.start_aware_datetime,
-                              finish_time=self.end_aware_datetime, status='pending',
-                              owner=self.user)
-        mailin.message.add(self.message)
-        mailin.save()
+        self.mailin = Mailin.objects.create(name='test_mail_1',
+                                            interval='daily',
+                                            start_time=start_aware_datetime,
+                                            finish_time=end_aware_datetime,
+                                            status='pending',
+                                            owner=self.user)
+        self.mailin.message.add(self.message)
+        self.mailin.save()
 
-        self.client = Client()
-        self.client.login(**{'email': 'testuser@mail.ru', 'password': 'pythonpass'})
+        self.test_client = Client.objects.create(name='test',
+                                                 email='test@gmail.com',
+                                                 owner=self.user)
+        self.test_client.mailin.add(self.mailin)
+        self.mailin.save()
 
     def test_mailing_list(self):
         response = self.client.get('/')
         assert response.status_code == 200
 
-    def test_mailing_update(self):
-
-        response = self.client.put(
-            '/update_mailing/test_mail_1/',
-            {
-                "status": "active",
-                "id_start_time": str(self.start_aware_datetime),
-                "finish_time": str(self.end_aware_datetime),
-                "interval": 0,
-                "message": 1,
-
+    def test_form_save_malin(self):
+        # Попытка создать объект рассылки с корректными данными
+        from_data = {
+            'name': 'test_mail_2',
+            'interval': 'daily',
+            'start_time_0': self.start_date_str,  # Дата для start_time
+            'start_time_1': self.start_time_str,  # Время для start_time
+            'finish_time_0': self.finis_date_str,  # Дата для finish_time
+            'finish_time_1': self.finis_time_str,  # Время для finish_time
             }
-        )
-        print(response.content, "содержимое ответа")
-        new_mailing = Mailin.objects.get(name='test_mail_1')
-        print(new_mailing.status, "статус")
-        assert response.status_code == 200
-        assert new_mailing.status == 'active'
-
-
-    def test_mailing_create(self):
-        response = self.client.post('/create_mailing/', {
-            'name': 'test_mail2',
-            'interval': 'ежедневно',
-            'start_time': self.start_aware_datetime,
-            'finish_time': self.end_aware_datetime,
-            'status': 'В ожидании',
-            'message': [self.message.id],
-            'owner': self.user.id,
-            },
-        )
-        assert response.status_code == 200
+        form = MailinCreateForm(data=from_data)
+        self.assertTrue(form.is_valid())
+        form.save()
         self.assertEqual(Mailin.objects.count(), 2)
 
     def test_create_mailin_with_invalid_data(self):
-        # Попытка создать объект с некорректными данными (например, пропущено обязательное поле)
-        response = self.client.post(reverse('mailin:create_mailing'), {
-            'name': 'test_mail',
+        # Попытка создать объект рассылки с некорректными данными (например, пропущено обязательное поле)
+        invalid_data = {
+            'name': '',
             'interval': 'daily',
-            'start_time': '2023-12-01 01:01',
-            'finish_time': '2023-12-01 01:02',
-            'status': 'pending',
-            # 'message': self.message.id,  # Отсутствует обязательное поле message
-            'owner': self.user.id
-        })
+            'start_time_0': self.start_date_str,  # Дата для start_time
+            'start_time_1': self.start_time_str,  # Время для start_time
+            'finish_time_0': self.finis_date_str,  # Дата для finish_time
+            'finish_time_1': self.finis_time_str,  # Время для finish_time
+            }
+        form = MailinCreateForm(data=invalid_data)
+        self.assertFalse(form.is_valid())  # Проверяет, что форма не является валидной
+        with self.assertRaises(ValueError):  # Проверяет, что при попытке сохранения формы возникнет исключение
+            form.save()
+        self.assertEqual(Mailin.objects.count(), 1)  # Объект не был создан. Есть только 1 объект из setUp
 
-        self.assertEqual(response.status_code, 200)  # Ожидается возвращение на страницу формы
-        self.assertEqual(Mailin.objects.count(), 1)  # Новый объект не должен быть создан
+    def test_detail_view(self):
+        # Попытка получить объект рассылки
+        response = self.client.get(reverse('mailin:detail_mailing', args=['test_mail_1']))
+        assert response.status_code == 200
+
+    def test_detail_view_anonymous(self):
+        # Попытка получить объект рассылки анонимом
+        self.client.logout()
+        response = self.client.get(reverse('mailin:detail_mailing', args=['test_mail_1']))
+        self.assertEqual(response.status_code, 302)  # Ожидает редирект, так как доступ без авторизации запрещен
+
+    def test_delete_mailing(self):
+        # Выполнение запроса на удаление рассылки
+        response = self.client.delete(reverse('mailin:delete_mailing', args=['test_mail_1']))
+
+        # проверка, что после удаления мы перенаправлены на ожидаемую страницу
+        self.assertRedirects(response, reverse('mailin:mailing_list'))
+
+        # проверка, что рассылка действительно удалена
+        self.assertEqual(Mailin.objects.filter(name='test_mail_1').exists(), False)
+
+    def test_delete_mailing_anonymous(self):
+        # Выполнение запроса на удаление рассылки анонимом
+        self.client.logout()
+        # выполнение запроса на удаление рассылки
+        response = self.client.delete(reverse('mailin:delete_mailing', args=['test_mail_1']))
+
+        # проверка, что рассылка действительно удалена
+        self.assertEqual(Mailin.objects.filter(name='test_mail_1').exists(), True)
--- mailing/views.py
+++ mailing/views.py
@@ -31,6 +31,7 @@ class MailinListView(LoginRequiredMixin, ListView):
 class MailinCreateView(LoginRequiredMixin, CreateView):
     model = Mailin
     success_url = reverse_lazy('mailin:mailing_list')  # Адрес для перенаправления после успешного создания
+    print('создание')
 
     def form_valid(self, form):
         user = self.request.user  # Получает текущего пользователя, который отправил форму
